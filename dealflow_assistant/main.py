be09022523b532e55df46a4f948957cac2683909
Nurkahfi Amran Rahmada <nurkahfirahmada@gmail.com>
2025-06-26 21:02:01 +0700

    fix: chronium error

--- dealflow_assistant/main.py
+++ dealflow_assistant/main.py
@@ -13,6 +13,9 @@ def scrape(request: ScrapeRequest):
     options = Options()
     
     options.add_argument("--headless")
+    options.add_argument("--headless")
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
     driver = webdriver.Chrome(options=options)
     
     results = []
